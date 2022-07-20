28f194a0cfe4ab73ed155af896a483b76aa9dc81
kspicer80 <50631380+kspicer80@users.noreply.github.com>
2022-07-20 06:28:25 -0500

    Update generating_fake_weird_fiction_data.py

--- weird_fiction_visualizations/generating_fake_weird_fiction_data.py
+++ weird_fiction_visualizations/generating_fake_weird_fiction_data.py
@@ -1,5 +1,6 @@
 import csv
 import pandas as pd
+pd.options.display.max_colwidth = 300
 from faker import Faker
 import datetime
 import random
@@ -9,46 +10,35 @@ semesters = ['Fall 2019', 'Fall 2020', 'Fall 2021', 'Fall 2022', 'Spring 2019', 
 author_list = ['Stephen King', 'Dino Buzzati', 'Jeff VanderMeer', 'Leonora Carrington', 'William Sansom']
 words = ['dog', 'cat', 'giraffe', 'elephant', 'amoeba', 'use', 'definition', 'HTML', 'css', 'python']
 
-random_author = str(random.choice(author_list))
-print(random_author)
-
-fake = Faker()
-Faker.seed(0)
+'''
+fake = Faker() 
+# Print random sentences 
+print(fake.sentence()) 
 
 sentences = []
-
-for _ in range(5):
-    random_author = str(random.choice(author_list))
-    sentences.append(fake.sentence(nb_words=10))
-    sentences.append(random_author)
-
-
-print(sentences)
-'''
-#print(random.choice(extra_word_list))
-
-def fake_data_generation(records):
-
-    fake = Faker('en-US')
+# Let's print 5 sentences that have words from our word_list  
+for _ in range(5): 
+    sentences.append(fake.sentence())
+    sentences.append(random.choice(author_list))
     
-    entry = []
+print(' '.join(sentences))
+'''
 
-    for i in range(records):
+def fake_data_generation(num_of_records):
+    fake = Faker('en-US')
+    entry = []
+    for i in range(num_of_records):
+        sentences = []
         first_name = fake.first_name()
         last_name = fake.last_name()
         semester = random.choice(semesters)
-        random_author = random.choice(extra_word_list)     
-        print(random_author)   
-        text = []
-        text.append(random_author)
-        print(text)                       
-        entry.append({'First Name': first_name, 'Last Name': last_name, 'Semester and Year': semester, 'Text of the Post': text})
+        sentences.append(fake.sentence(10))
+        sentences.append(random.choice(author_list))
+        joined_sentence = ' '.join(sentences)
+        entry.append({'First Name': first_name, 'Last Name': last_name, 'Semester and Year': semester, 'Text of the Post': joined_sentence})
     return(entry)
 
 fake_df = pd.DataFrame(fake_data_generation(10))
-#print(fake_df['Text of the Post'].head())
+print(fake_df['Text of the Post'].head())
 
-matches = fake_df[fake_df['Text of the Post'].isin(['Stephen King', 'Dino Buzzati', 'Jeff VanderMeer', 'Leonora Carrington', 'William Sansom'])]
-print(matches)
-'''
 
