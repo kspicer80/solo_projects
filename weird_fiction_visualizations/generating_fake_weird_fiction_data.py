9b73a309f5be99e0fcad5470b18d913ae7eaaec1
kspicer80 <50631380+kspicer80@users.noreply.github.com>
2022-07-20 08:52:37 -0500

    update fake_data script

--- weird_fiction_visualizations/generating_fake_weird_fiction_data.py
+++ weird_fiction_visualizations/generating_fake_weird_fiction_data.py
@@ -4,29 +4,14 @@ pd.options.display.max_colwidth = 300
 from faker import Faker
 import datetime
 import random
-from faker.providers import BaseProvider
+from pathlib import Path
 
 semesters = ['Fall 2019', 'Fall 2020', 'Fall 2021', 'Fall 2022', 'Spring 2019', 'Spring 2020', 'Spring 2021', 'Spring 2022']
 author_list = ['Stephen King', 'Dino Buzzati', 'Jeff VanderMeer', 'Leonora Carrington', 'William Sansom']
-words = ['dog', 'cat', 'giraffe', 'elephant', 'amoeba', 'use', 'definition', 'HTML', 'css', 'python']
-
-'''
-fake = Faker() 
-# Print random sentences 
-print(fake.sentence()) 
-
-sentences = []
-# Let's print 5 sentences that have words from our word_list  
-for _ in range(5): 
-    sentences.append(fake.sentence())
-    sentences.append(random.choice(author_list))
-    
-print(' '.join(sentences))
-'''
 
 def fake_data_generation(num_of_records):
     fake = Faker('en-US')
-    entry = []
+    dataframe = []
     for i in range(num_of_records):
         sentences = []
         first_name = fake.first_name()
@@ -35,10 +20,11 @@ def fake_data_generation(num_of_records):
         sentences.append(fake.sentence(10))
         sentences.append(random.choice(author_list))
         joined_sentence = ' '.join(sentences)
-        entry.append({'First Name': first_name, 'Last Name': last_name, 'Semester and Year': semester, 'Text of the Post': joined_sentence})
-    return(entry)
+        dataframe.append({'First Name': first_name, 'Last Name': last_name, 'Semester and Year': semester, 'Text of the Post': joined_sentence})
+    return(dataframe)
 
-fake_df = pd.DataFrame(fake_data_generation(10))
-print(fake_df['Text of the Post'].head())
+fake_df = pd.DataFrame(fake_data_generation(100))
+fake_df.to_csv('fake_dataset_csv.csv', index=False)
+
 
 
