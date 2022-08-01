b674421bf8328f3f973f8b3ad6f357608d2ada66
kspicer80 <50631380+kspicer80@users.noreply.github.com>
2022-08-01 14:10:38 -0500

    more updates

--- sqlite_in_python_tutorial/sqlite_practice.py
+++ sqlite_in_python_tutorial/sqlite_practice.py
@@ -25,17 +25,40 @@ c.executemany("INSERT INTO customers VALUES (?, ?, ?)", many_customers)
 '''
 
 # Querying the Database
-c.execute("SELECT * FROM customers")
+#c.execute("SELECT * FROM customers")
+#c.execute("SELECT rowid, * FROM customers")
+#c.execute("SELECT * FROM customers WHERE last_name = 'Granger'")
+#c.execute("SELECT * FROM customers WHERE last_name LIKE 'Wee%'")
+#c.execute("SELECT * FROM customers WHERE e_mail LIKE '%hogwarts.com'")
 #print(c.fetchone()) # fetch one
 #c.fetchmany(2) # fetch two 
 #print(c.fetchall()) # fetch all
 
-items = c.fetchall()
+# How do we update records ...?
+#c.execute("""UPDATE customers SET first_name = 'Joe'
+ #           WHERE rowid=1
+  #          """)
+#conn.commit()
+
+# How do we delete records ...?
+#c.execute("DELETE from customers WHERE rowid = 1")
+
+#c.execute("SELECT rowid, * FROM customers")
+#c.execute("SELECT rowid, * FROM customers")
+
+
 #print("NAME " + "\t\tEMAIL")
 #print("----" + "\t\t-----" )
-for item in items:
-    print(item[0] + " " + item[1] + " " + item[2])
+#for item in items:
+    #print(item[0] + " " + item[1] + " " + item[2])
 
+# Ordering Results
+c.execute("SELECT rowid, * FROM customers ORDER BY rowid DESC")
+items = c.fetchall()
+for item in items:
+    print(item)
+
+# Searching for Specific Things
 print("Commands executed successfully ...")
 conn.commit()
 conn.close()
