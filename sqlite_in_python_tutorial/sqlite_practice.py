7a8d6e5b5929a76ffbaa6129fe44ac4440a7e982
kspicer80 <50631380+kspicer80@users.noreply.github.com>
2022-08-01 13:47:13 -0500

    updates

--- sqlite_in_python_tutorial/sqlite_practice.py
+++ sqlite_in_python_tutorial/sqlite_practice.py
@@ -1,21 +1,41 @@
 import sqlite3
 
-conn = sqlite3.connect('customer.db')
+conn = sqlite3.connect('customers.db')
 
 c = conn.cursor()
 
-# Inserting One Record at a Time
+# Creating the Table:
+#c.execute("""CREATE TABLE customers(
+    #first_name text,
+    #last_name text,
+    #e_mail text)""")
+
+# Inserting a Single Record into the Database
+'''
 c.execute("INSERT INTO customers VALUES ('Mary', 'Blow', 'mb@test.com')")
 
 # Inserting multiple records at once:
-
 many_customers = [
-    ('Wes', 'Brown', 'wb@example.com'),
-    ('Stephanie', 'Brown', 'sb@example.com'),
+    ('Hermione', 'Granger', 'hg@hogwarts.com'),
+    ('Ronald', 'Weesley', 'rw@hogwarts.com'),
     ('Harry', 'Potter', "hp@hogwarts.com")
 ]
 
 c.executemany("INSERT INTO customers VALUES (?, ?, ?)", many_customers)
+'''
 
+# Querying the Database
+c.execute("SELECT * FROM customers")
+#print(c.fetchone()) # fetch one
+#c.fetchmany(2) # fetch two 
+#print(c.fetchall()) # fetch all
+
+items = c.fetchall()
+#print("NAME " + "\t\tEMAIL")
+#print("----" + "\t\t-----" )
+for item in items:
+    print(item[0] + " " + item[1] + " " + item[2])
+
+print("Commands executed successfully ...")
 conn.commit()
 conn.close()
